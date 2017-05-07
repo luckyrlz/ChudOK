b041aff41d3870c7744b99f3e482ed03a5f6bd3b
Sergey Ushakov <sushakov@Sergeys-MacBook-Pro.local>
2017-05-07 11:23:49 +0300

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {
+  AppRegistry
+} from 'react-native';
+import App from './src/App';
+
+export default class ChudOK extends Component {
+  render() {
+    return (
+        <App />
+    );
+  }
+}
+
+AppRegistry.registerComponent('ChudOK', () => ChudOK);
---
+++ src/components/ViewContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet
+} from 'react-native';
+
+export default class ViewContainer extends Component {
+  render() {
+    return (
+      <View style={styles.ViewContainer}>
+
+      </View>
+    );
+  }
+}
